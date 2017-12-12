8977bbe8d7f8a10a25d32faa1ebfb927e310e992
Luís Rosa <luis.rosa@philips.com>
2017-12-12 13:06:10 +0100

    First commit

---
+++ Digidentity/DigidentityTests/DigidentityTests.swift
@@ -0,0 +1,36 @@
+//
+//  DigidentityTests.swift
+//  DigidentityTests
+//
+//  Created by luis.rosa@philips.com on 12/12/2017.
+//  Copyright © 2017 Luis. All rights reserved.
+//
+
+import XCTest
+@testable import Digidentity
+
+class DigidentityTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
